bf112e692f89ca28e3e4db7002671afd89e1f6f1
lamminhdat2k3@gmail.com <lamminhdat2k3@gmail.com>
2023-10-25 16:58:15 +0700

    init project seasion 2

---
+++ AppQL/app/dao.py
@@ -0,0 +1,38 @@
+def get_categories():
+    return [{
+        "id": 1,
+        "name": "Mobile"
+    },{
+        "id": 2,
+        "name": "Tablet"
+    }]
+
+def get_products():
+    products = [{
+        "id": 1,
+        "name": "Iphone 13",
+        "price": 20000000 ,
+        "image":"https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    },{
+        "id": 2,
+        "name": "Galaxy S23",
+        "price": 15000000 ,
+        "image":"https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    },{
+        "id": 3,
+        "name": "Iphone 14",
+        "price": 25000000 ,
+        "image":"https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    },{
+        "id": 4,
+        "name": "Iphone 15",
+        "price": 35000000 ,
+        "image":"https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    },{
+        "id": 5,
+        "name": "Apple watch",
+        "price": 10000000 ,
+        "image":"https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    }]
+
+    return products
